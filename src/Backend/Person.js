856f8de8cd3266e76f381332b1a809b885131b30
hamzahalazzeh <115584906+hamzahalazzeh@users.noreply.github.com>
2022-11-04 12:42:14 -0700

    dmv class added
    dmv class added

--- src/Backend/Person.js
+++ src/Backend/Person.js
@@ -1,5 +1,89 @@
 import {getPerson} from './SSVendia'
 import {useState} from 'react'
+
+
+class DMV {
+    constructor(targetPerson){
+        this.photo(targetPerson.photo);
+        this.dlNum(targetPerson.dlNum);
+        this.fName(targetPerson.fName);
+        this.mName(targetPerson.MName);
+        this.lName(targetPerson.LName);
+        this.birthDay(targetPerson.BirthDay);
+        this.birthMonth(targetPerson.BirthMonth);
+        this.birthYear(targetPerson.BirthYear);
+    }
+
+    set photo(photo){
+        this.photo = photo;
+    }
+
+    set dlNum(dlNum){
+        this.dlNum = dlNum;
+    }
+
+    set fName(fName){
+        this.fName = fName;
+    }
+
+    set mName(mName){
+        this.mName = mName;
+    }
+
+    set lName(lName){
+        this.lName = lName;
+    }
+
+    set birthDay(birthDay){
+        this.birthDay = birthDay;
+    }
+
+    set birthMonth(birthMonth){
+        this.birthMonth = birthMonth;
+    }
+
+    set birthYear(birthYear){
+        this.birthYear = birthYear
+    }
+
+    get photo() {
+        return this.photo;
+    }
+
+    get dlNum() {
+        return this.dlNum;
+    }
+
+    get FName() {
+        return this.fName;
+    }
+
+    get MName() {
+        return this.mName;
+    }
+
+    get LName() {
+        return this.lName;
+    }
+
+    get FullName() {
+        return this.fName + " " + this.mName + " " + this.lName;
+    }
+
+    get BirthDay() {
+        return this.birthDay;
+    }
+
+    get BirthMonth() {
+        return this.birthMonth;
+    }
+
+    get BirthYear() {
+        return this.birthYear;
+    }
+}
+
+
 class SocialSecurity {
     constructor(targetPerson){
         alert("In constructor");
